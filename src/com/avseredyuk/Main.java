06879d3c14004fbaefe8720396397863ce09aa8e
avsereduik <a.v.seredyuk@gmail.com>
2017-03-06 16:26:20 +0200

    added smth

--- src/com/avseredyuk/Main.java
+++ src/com/avseredyuk/Main.java
@@ -1,24 +1,64 @@
 package com.avseredyuk;
 
+import com.avseredyuk.domain.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 /**
  * Created by Anton_Serediuk on 3/6/2017.
  */
 public class Main {
+    static final String STRING_FORMAT_PRODUCTS =
+            "\t%d\t\t%d\t\t%s";
+    static final String PRODUCTS_LIST_HEADER =
+            "\tID\t\tPRICE\tNAME";
+    static final String STRING_FORMAT_ERROR =
+            "Error happened: %s";
     static final String[] COMMANDS_ARRAY =
-            {"list"};
+            {"list", "add"};
+    static final List<Product> products = new ArrayList<Product>(){{
+        add(new Product(25, "Tea", 1));
+        add(new Product(35, "Coffee", 2));
+        add(new Product(45, "Juice", 3));}};
 
     public static void main(String[] args) {
-
-
-
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String params[] = scanner.nextLine().split(" ");
             if (params.length > 0) {
                 if (params[0].equals(COMMANDS_ARRAY[0])) {
-                    // list smth
+                    System.out.println(PRODUCTS_LIST_HEADER);
+                    for (Product p : products) {
+                        System.out.println(String.format(STRING_FORMAT_PRODUCTS,
+                                p.getId(),
+                                p.getPrice(),
+                                p.getName()));
+                    }
+                } else if (params[0].equals(COMMANDS_ARRAY[1])) {
+                    if (params.length == 2) {
+                        try {
+                            String idString = params[1];
+                            int id = Integer.valueOf(idString);
+                            Optional<Product> optionalProduct = products.stream()
+                                    .filter(p -> p.getId() == id)
+                                    .findFirst();
+                            if (optionalProduct.isPresent()) {
+                                Product product = optionalProduct.get();
+
+                                //todo smth
+
+
+                            } else {
+                                throw new NumberFormatException();
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println(String.format(STRING_FORMAT_ERROR,
+                                    "Illegal input"));
+                        }
+                    }
                 }
             }
         }
---
+++ src/com/avseredyuk/domain/Product.java
@@ -0,0 +1,28 @@
+package com.avseredyuk.domain;
+
+/**
+ * Created by Anton_Serediuk on 3/6/2017.
+ */
+public class Product {
+    private int price;
+    private String name;
+    private int id;
+
+    public Product(int price, String name, int id) {
+        this.price = price;
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
